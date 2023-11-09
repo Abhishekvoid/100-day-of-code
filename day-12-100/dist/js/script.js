7bdce306ff7337c5dd7838d660573cc34c73d46f
Abhishekvoid <91648941+Abhishekvoid@users.noreply.github.com>
2023-11-09 22:37:53 +0530

    day-12

---
+++ day-12-100/dist/js/script.js
@@ -0,0 +1,17 @@
+
+$(".for-slider").slick({
+    slidesToShow: 1,
+    autoplay: true,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    arrows: true,
+
+
+    prevArrow: "<span class ='slick-prevarrow d-flex align-item-center justify-content-center position-relative'> <svg class='icon'> <use xlink:href='png/allsvg.svg#left-arrow'></use></svg>",
+    nextArrow: "<span class='slick-nextarrow d-flex align-item-center justify-content-center position-relative'><svg class='next_icon'><use xlink:href='png/allsvg.svg#right-arrow'></use></svg>",
+
+
+
+});
