4c69fc4d4908a9e1e2b27664eee89c89d0e49829
Abhishekvoid <91648941+Abhishekvoid@users.noreply.github.com>
2023-11-11 19:14:36 +0530

    day-14

---
+++ day-14-100/script.js
@@ -0,0 +1,43 @@
+let variable=3;
+console.log(variable);
+
+
+let calculation = 2+2;
+console.log(calculation);
+
+let result = calculation + variable + 2;
+console.log(result)
+
+
+let CartQuantity = 0;
+
+true
+false
+console.log(3 > 5);
+
+console.log( 5 == '5.000');
+console.log( 5 == 'hello');
+
+console.log(5 === '5.000')
+console.log( 5 === 5);
+
+if (true){
+        console.log('hello')
+}
+
+const age =15;
+
+if( age >= 16){
+    console.log('You can drive')
+}
+else if (age >= 14){
+    console.log('Alomst there! ')
+}
+else {
+    console.log('You can not drive')
+}
+
+
+const ans = true ? 'truthy' : 'falsey';
+console.log(ans);
+
