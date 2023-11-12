faa0980dbd60060b15db65e6e0dea61a2e7a1a56
Abhishekvoid <91648941+Abhishekvoid@users.noreply.github.com>
2023-11-12 22:06:31 +0530

    day-15

---
+++ day-15-100/script.js
@@ -0,0 +1,21 @@
+/* Function declarations
+A function definition (also called a function declaration, or function statement) consists of the function keyword, followed by:
+
+The name of the function.
+A list of parameters to the function, enclosed in parentheses and separated by commas.
+The JavaScript statements that define the function, enclosed in curly braces, {  }. 
+For example, the following code defines a simple function named square: */
+
+function sqaure(numbers){
+    return number * number;
+}
+
+/* The function square takes one parameter, called number. The function consists of one statement
+ that says to return the parameter of the function (that is, number) multiplied by itself. The return 
+ statement specifies the value returned by the function, which is number * number.*/
+
+function print(){
+    console.log('Hello');
+    console.log(2 + 2);
+}
+print();
