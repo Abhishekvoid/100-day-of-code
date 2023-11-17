62b91edf9e47761b2dcc2225ffbbb529a29d4402
Abhishekvoid <91648941+Abhishekvoid@users.noreply.github.com>
2023-11-17 22:09:01 +0530

    day-20

---
+++ day-20-100/adv-DOM.js
@@ -0,0 +1,18 @@
+//GetElementById()
+
+
+
+
+
+//querySelector()
+
+const title = document.querySelector('#main-heading');
+title.style.color = 'red'
+//queryselectorAll()
+
+const ul = document.querySelector('ul');
+const li = document.querycreateElement('li');
+
+ul.append(li)
+
+console.log();
