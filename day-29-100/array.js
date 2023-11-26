b1b21db300823d3244b1ce547c6c53b89d552907
Abhishekvoid <91648941+Abhishekvoid@users.noreply.github.com>
2023-11-26 22:08:09 +0530

    day-29

---
+++ day-29-100/array.js
@@ -0,0 +1,10 @@
+const friends = ['abhi', 'yyoy', 'tinf']
+console.log(friends);
+
+
+const years = [1991, 1992, 1993, 1994]
+console.log(years);
+
+
+console.log(friends[2]);
+console.log(friends.length);
